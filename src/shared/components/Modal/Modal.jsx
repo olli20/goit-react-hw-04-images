d400dcd401bc385516bc1ec64daad105a09dad2b
Olena Voina <105532756+olli20@users.noreply.github.com>
2023-02-06 12:43:06 +0100

    Modal

--- src/shared/components/Modal/Modal.jsx
+++ src/shared/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import {useEffect} from 'react';
 import {createPortal} from 'react-dom';
 import PropTypes from 'prop-types';
 
@@ -8,37 +8,29 @@ import styles from './modal.module.scss';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-
-    componentDidMount() {
-        document.addEventListener("keydown", this.closeModal);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener("keydown", this.closeModal);
-    }
-
-    closeModal = ({target, currentTarget, code}) => {
+const Modal = ({onClose, children}) => {
+    const close = ({target, currentTarget, code}) => {
         if(target === currentTarget || code === "Escape") {   
-            this.props.onClose();
+            onClose();
         }
     }
 
-    render() {
-        const {children, onClose} = this.props;
-        const {closeModal} = this;
-        return (
-            createPortal(
-                <div onClick={closeModal} className={styles.overlay}>
-                    <div className={styles.modal}>
-                        <IoCloseCircleOutline className={styles.close} onClick={onClose} />
-                        {children}
-                    </div>
-                </div>,
-                modalRoot
-            )
+    useEffect(() => {
+        document.addEventListener("keydown", close);
+        return () => document.removeEventListener("keydown", close);
+    }, [close]);
+
+    return (
+        createPortal(
+            <div onClick={close} className={styles.overlay}>
+                <div className={styles.modal}>
+                    <IoCloseCircleOutline className={styles.close} onClick={onClose} />
+                    {children}
+                </div>
+            </div>,
+            modalRoot
         )
-    }
+    )
 }
 
 export default Modal;
@@ -47,3 +39,57 @@ Modal.propTypes = {
     onClose: PropTypes.func.isRequired,
     children: PropTypes.element.isRequired,
 }
+
+
+
+
+
+// import {Component} from 'react';
+// import {createPortal} from 'react-dom';
+// import PropTypes from 'prop-types';
+
+// import { IoCloseCircleOutline } from "react-icons/io5";
+
+// import styles from './modal.module.scss';
+
+// const modalRoot = document.querySelector('#modal-root');
+
+// class Modal extends Component {
+
+//     componentDidMount() {
+//         document.addEventListener("keydown", this.closeModal);
+//     }
+
+//     componentWillUnmount() {
+//         document.removeEventListener("keydown", this.closeModal);
+//     }
+
+//     closeModal = ({target, currentTarget, code}) => {
+//         if(target === currentTarget || code === "Escape") {   
+//             this.props.onClose();
+//         }
+//     }
+
+//     render() {
+//         const {children, onClose} = this.props;
+//         const {closeModal} = this;
+//         return (
+//             createPortal(
+//                 <div onClick={closeModal} className={styles.overlay}>
+//                     <div className={styles.modal}>
+//                         <IoCloseCircleOutline className={styles.close} onClick={onClose} />
+//                         {children}
+//                     </div>
+//                 </div>,
+//                 modalRoot
+//             )
+//         )
+//     }
+// }
+
+// export default Modal;
+
+// Modal.propTypes = {
+//     onClose: PropTypes.func.isRequired,
+//     children: PropTypes.element.isRequired,
+// }
